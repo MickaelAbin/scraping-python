7e7cd21fb21fd3a5c67dc572c996e52fc824a00d
makel44 <120366866+makel44@users.noreply.github.com>
2024-04-09 09:43:08 +0200

    résultat

--- main.py
+++
@@ -1,8 +0,0 @@
-from Chat import *
-
-if __name__ == '__main__':
-    c = Chat("Minou")
-    print(f"The cat's name is {c.name}")
-    print(f"cat is {c.state}")
-    c.sleep()
-    print(f"cat is {c.state}")
